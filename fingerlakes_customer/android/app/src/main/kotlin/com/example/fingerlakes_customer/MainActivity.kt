ce107da315ab649c245c2a2926e1fbbabb32b17d
Vespir Bowman <acafecat@gmail.com>
2026-06-18 09:34:40 -0400

    Folders Setup

---
+++ fingerlakes_customer/android/app/src/main/kotlin/com/example/fingerlakes_customer/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.fingerlakes_customer
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
---
+++ fingerlakes_store/android/app/src/main/kotlin/com/example/fingerlakes_store/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.fingerlakes_store
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
